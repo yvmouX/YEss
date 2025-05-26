63b0e690e919af0527a18ea57318550cebef1128
yvmou_ <yvmou@outlook.com>
2025-05-26 18:19:39 +0800

    3

--- src/main/java/cn/yvmou/yess/commands/main/MainCommand.java
+++ src/main/java/cn/yvmou/yess/commands/main/MainCommand.java
@@ -7,7 +7,6 @@ import cn.yvmou.yess.commands.main.sub.ReloadCmd;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,14 +22,26 @@ public class MainCommand implements CommandExecutor {
         subCommands.put("reload", new ReloadCmd());
     }
 
-    @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        return false;
-    }
+
 
     private void sendVersionMessage(CommandSender sender) {
         sender.sendMessage("YEss version: " + plugin.getDescription().getVersion());
     }
 
-
+    /**
+     * Executes the given command, returning its success.
+     * <br>
+     * If false is returned, then the "usage" plugin.yml entry for this command
+     * (if defined) will be sent to the player.
+     *
+     * @param sender  Source of the command
+     * @param command Command which was executed
+     * @param label   Alias of the command which was used
+     * @param args    Passed command arguments
+     * @return true if a valid command, otherwise false
+     */
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
 }
